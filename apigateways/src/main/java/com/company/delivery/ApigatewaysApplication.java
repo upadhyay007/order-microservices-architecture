142e82dba70d9f727e09e20ea253fd46ff5c42f5
Anurag upadhyay <anuragupadhyayblogs@gmail.com>
2022-12-12 20:01:08 +0530

    initial commit order micro-services

---
+++ apigateways/src/main/java/com/company/delivery/ApigatewaysApplication.java
@@ -0,0 +1,13 @@
+package com.company.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewaysApplication.class, args);
+	}
+
+}
---
+++ user-service/src/main/java/com/company/delivery/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.company.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
