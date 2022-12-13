9b8c237a1b7d8b610e419782428be6ffe9728982
Anurag upadhyay <anuragupadhyayblogs@gmail.com>
2022-12-13 22:15:10 +0530

    package name corrected

--- account-service/src/main/java/com/company/delivery/AccountServiceApplication.java
+++ account-service/src/main/java/com/company/delivery/AccountServiceApplication.java
@@ -1,4 +1,4 @@
-package com.company.delivery.accountservice;
+package com.company.delivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
